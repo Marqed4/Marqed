0dd2b2572f75e52be8411f4463cec6b046f85a14
Marqed <Derpexsplosion@gmail.com>
2026-03-13 15:48:24 -0400

    Fixing Background

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from "./Navbar"
+import OrangeBlobBackground from "../Resources/art/Background Project/Orange Blob.gif"
 
 const Home = () => {
   return ( 
   <div className = "home-container">
     <Navbar/>
     <div className = "home-banner-container">
-        <div className = "home-bannerImage-container">
-        <img src = {bannerbackground} alt = "" />
+        <div className = "home-OrangeBlobBackground-container">
+        <img src = {OrangeBlobBackground} alt = "" />
+        </div>
+        <div className = "none-text-section" >
+            <h1 classBame = "primary-heading">
+
+            </h1>
         </div>
     </div>
     </div>
