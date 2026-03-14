6b2076f88cbb765d6e4fe03f2fec57807863a4d5
Marqed <Derpexsplosion@gmail.com>
2026-03-14 17:14:21 -0400

    Further Customization

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,38 @@
 import "../Components/Home.js"
 import Navbar from "./Navbar.js"
-import bannerbackground from "../Resources/art/Background Project/Orange Glob.png"
-// import bannerbackground from "../Resources/art/Background Project/Orange Glob Animations.mp4"
+import bannerbackground from "../Resources/art/Background Project/Orange Glob.gif"
+import { Gif } from "@mui/icons-material";
 
 const Home = () => {
   return ( 
-  <div className = "home-container">
-    <Navbar/>
-    <div className = "home-banner-container">
-        <div className = "home-bannerImage-container">
-        <img src = {bannerbackground} alt = "" />
-        </div>
+    <div className="home-container">
+      <Navbar/>
+      <div className="home-banner-container">
+        <img src={bannerbackground} alt="" />
+      </div>
+      <div className="home-name-text-container">
+        <h1>Zachery</h1>
+        <p>
+        I'm a video editor and developer based out of New York. <br />
+        I make YouTube edits, montages, and build things on the web. <br />
+        This is my corner of the internet — have a look around.
+        </p>
+      </div>
     </div>
-  </div>
   );
 };
 
 export default Home;
+
+
+// how to play video
+      //  <video 
+      //     autoPlay 
+      //     muted 
+      //     loop 
+      //     playsInline 
+      //     className="background-video"
+      //   >
+      //     <source src={bannerbackground} type="video/mp4" />
+      //     Your browser does not support the video tag.
+      //     </video>
