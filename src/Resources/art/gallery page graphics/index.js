bed8e71ae09d5e5ca3b2b5c8b8cf1c9e250c5d44
Marqed <Derpexsplosion@gmail.com>
2026-07-21 12:58:22 -0400

    Adding Great Company!

--- src/Resources/art/gallery page graphics/index.js
+++ src/Resources/art/gallery page graphics/index.js
@@ -4,7 +4,8 @@ import IMG_0510 from "./IMG_0510.jpg"
 import IMG_1213 from "./IMG_1213.jpg"
 import IMG_1222 from "./IMG_1222.jpg"
 import IMG_1309 from "./IMG_1309.jpg"
+import IMG_1336 from "./IMG_1336.jpg"
 
 export const PetGallery = {
-    IMG_0510, IMG_0528, IMG_1309, IMG_0987, IMG_1213, IMG_1222,
+    IMG_0510, IMG_0528, IMG_1309, IMG_0987, IMG_1213, IMG_1222, IMG_1336,
 }
